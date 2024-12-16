ba45fa997ea1c232b4cd9629fbd41e96fe337b48
Jan Kruchina <jan@kruchina.cz>
2024-12-16 22:17:38 +0100

    changed the order of format specifiers in process_format_specifier function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jakruchi <jakruchi@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 20:29:41 by jakruchi          #+#    #+#             */
-/*   Updated: 2024/12/16 21:25:05 by jakruchi         ###   ########.fr       */
+/*   Updated: 2024/12/16 22:10:12 by jakruchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,22 @@ int	process_format_specifier(const char *str, va_list arg)
 	int	count;
 
 	count = 0;
-	if (*(str + 1) == 'd' || *(str + 1) == 'i')
-		count = put_nbr(va_arg(arg, int));
+	if (*(str + 1) == 'c')
+		count = put_char(va_arg(arg, int));
 	else if (*(str + 1) == 's')
 		count = put_str(va_arg(arg, char *));
-	else if (*(str + 1) == 'c')
-		count = put_char(va_arg(arg, int));
-	else if (*(str + 1) == '%')
-		count = put_char('%');
+	else if (*(str + 1) == 'p')
+		count = put_pointer(va_arg(arg, void *));
+	else if (*(str + 1) == 'd' || *(str + 1) == 'i')
+		count = put_nbr(va_arg(arg, int));
 	else if (*(str + 1) == 'u')
 		count = put_nbr(va_arg(arg, unsigned int));
 	else if (*(str + 1) == 'x')
 		count = put_hex(va_arg(arg, unsigned int), "0123456789abcdef");
 	else if (*(str + 1) == 'X')
 		count = put_hex(va_arg(arg, unsigned int), "0123456789ABCDEF");
-	else if (*(str + 1) == 'p')
-		count = put_pointer(va_arg(arg, void *));
+	else if (*(str + 1) == '%')
+		count = put_char('%');
 	else
 		count = write(1, str + 1, 1);
 	return (count);
